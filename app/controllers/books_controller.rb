3dc5a495eaf2fee83327c7d76384042bdaa79b4c
beccaelenzil <beccaelenzil@gmail.com>
2020-04-22 11:18:40 -0700

    updated books_controller actions to point to database

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,11 @@
-BOOKS = [
-  { title: "Hidden Figures", author: "Margot Lee Shetterly"},
-  { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
-  { title: "Kindred", author: "Octavia E. Butler"}
-]
-
 class BooksController < ApplicationController
   def index
-    @books = BOOKS
+    @books = Book.all
   end
 
   def show
-    book_id = params[:id].to_i
-    @book = BOOKS[book_id]
+    book_id = params[:id]
+    @book = Book.find_by(id: book_id)
     if @book.nil?
       head :not_found
       return
