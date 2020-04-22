9cf2534ea8bdf4bd7f64b48fa0766e57ef050649
beccaelenzil <beccaelenzil@gmail.com>
2020-04-22 08:34:42 -0700

    completed index action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,11 @@
+BOOKS = [
+  { title: "Hidden Figures", author: "Margot Lee Shetterly"},
+  { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
+  { title: "Kindred", author: "Octavia E. Butler"}
+]
+
 class BooksController < ApplicationController
   def index
+    @books = BOOKS
   end
 end
