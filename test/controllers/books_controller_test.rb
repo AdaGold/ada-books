5c56dd8d6aae0bf5c488c14d15f968790c559c86
beccaelenzil <beccaelenzil@gmail.com>
2020-04-22 10:23:46 -0700

    show action tests

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,8 +1,35 @@
 require "test_helper"
 
 describe BooksController do
-  it "should get index" do
-    get "/books"
-    must_respond_with :success
+  describe "index" do
+    it "should get index" do
+      get "/books"
+      must_respond_with :success
+    end
   end
+
+  describe "show" do
+    it "will get show for valid ids" do
+      # Arrange
+      valid_book_id = 1
+  
+      # Act
+      get "/books/#{valid_book_id}"
+  
+      # Assert
+      must_respond_with :success
+    end
+  
+    it "will respond with not_found for invalid ids" do
+      # Arrange
+      invalid_book_id = 999
+  
+      # Act
+      get "/books/#{invalid_book_id}"
+  
+      # Assert
+      must_respond_with :not_found
+    end
+  end
+
 end
