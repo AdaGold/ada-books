c3175007d407ba1ed325f5804048f9740dbe5b4e
beccaelenzil <beccaelenzil@gmail.com>
2020-04-24 07:36:30 -0700

    create action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,5 +16,17 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+
+  # in app/controllers/books_controller.rb
+  def create
+    @book = Book.new(author: params[:book][:author], title: params[:book][:title], description: params[:book][:description]) #instantiate a new book
+    if @book.save # save returns true if the database insert succeeds
+      redirect_to books_path # go to the index so we can see the book in the list
+      return
+    else # save failed :(
+      render :new # show the new book form view again
+      return
+    end
+  end
   
 end
