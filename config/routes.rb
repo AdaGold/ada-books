3a48098e540a6db7c40c29a090e5e6ed8ffffb2e
beccaelenzil <beccaelenzil@gmail.com>
2020-04-24 07:19:00 -0700

    added restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 # config/routes.rb
 Rails.application.routes.draw do
   # verb 'path', to: 'controller#action'
-  get '/books', to: 'books#index'
-  get '/books/:id', to: 'books#show'
+
+  # Routes that operate on the book collection
+  get '/books', to: 'books#index', as: 'books'
+  get '/books/new', to: 'books#new', as: 'new_book'
+  post '/books', to: 'books#create'
+
+  # Routes that operate on individual books
+  get '/books/:id', to: 'books#show', as: 'book'
+  get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
+  patch '/books/:id', to: 'books#update'
+  delete '/books/:id', to: 'books#destroy'
 end
