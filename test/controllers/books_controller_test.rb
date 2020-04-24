6c273b8648acfceb6d701648e0afb954ebcc78d6
beccaelenzil <beccaelenzil@gmail.com>
2020-04-24 11:02:25 -0700

    update tests

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -60,4 +60,46 @@ describe BooksController do
         # fill this in when we implement validations next week
     end
   end
+
+  describe "update" do
+    before do
+      Book.create(title: "We're all wonders", author: " R.J. Palacio", description: "Good kids book")
+    end
+    let (:new_book_hash) {
+      {
+        book: {
+          title: "A Wrinkle in Time",
+          author: "Madeleine L'Engle",
+          description: "A fabulous adventure",
+        },
+      }
+    }
+    it "will update a model with a valid post request" do
+      id = Book.first.id
+      expect {
+        patch book_path(id), params: new_book_hash
+      }.wont_change "Book.count"
+  
+      must_respond_with :redirect
+  
+      book = Book.find_by(id: id)
+      expect(book.title).must_equal new_book_hash[:book][:title]
+      expect(book.author).must_equal new_book_hash[:book][:author]
+      expect(book.description).must_equal new_book_hash[:book][:description]
+    end
+  
+    it "will respond with not_found for invalid ids" do
+      id = -1
+  
+      expect {
+        patch book_path(id), params: new_book_hash
+      }.wont_change "Book.count"
+  
+      must_respond_with :not_found
+    end
+  
+    it "will not update if the params are invalid" do
+      # This test will be examined when we cover validations next week
+    end
+  end
 end
