e93702a1df888b95c39a291955e99b94707dab56
CheezItMan <chris@adadevelopersacademy.org>
2020-05-03 19:47:09 -0700

    add first_published method  in author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,9 @@
 class Author < ApplicationRecord
   has_many :books
+
+  def first_published
+    books_with_year = self.books.where.not(publication_date: nil)
+    first_book = books_with_year.order(publication_date: :asc).first
+    return first_book.publication_date
+  end
 end
