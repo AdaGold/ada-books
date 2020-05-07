68fe6e6d0f9227e8dcadbb255a80640a7d2c573f
dee del rosario <dee@adadevelopersacademy.org>
2020-05-07 16:41:08 -0700

    implements current user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,11 @@ class UsersController < ApplicationController
   end
 
   def current
-    
+    @current_user = User.find_by(id: session[:user_id])
+    unless @current_user
+      flash[:error] = "You must be logged in to see this page"
+      redirect_to root_path
+      return
+    end
   end
 end
