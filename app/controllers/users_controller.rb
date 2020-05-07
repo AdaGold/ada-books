f8136d50ee4340cc7dfd57f3ff0be632197684e4
dee del rosario <dee@adadevelopersacademy.org>
2020-05-07 16:31:01 -0700

    implements login form for logging in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
 
   def login_form
-    
+    @user = User.new
   end
 
   def login
-    
+    username = params[:user][:username]
+    user = User.find_by(username: username)
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as returning user #{username}"
+    else
+      user = User.create(username: username)
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as new user #{username}"
+    end
+
+    redirect_to root_path
+    return
   end
 
   def logout
