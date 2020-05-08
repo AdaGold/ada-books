8364b5a837bb9f68a32e8287c42813002717be21
dee del rosario <dee@adadevelopersacademy.org>
2020-05-08 12:06:40 -0700

    tests that a user can login and go to the current user page

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 describe UsersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "current" do
+    it "returns 200 OK for a logged-in user" do
+      # Arrange
+      user = User.first
+      login_data = {
+        user: {
+          username: user.username
+        }
+      }
+
+      post login_path, params: login_data
+
+      # Verify the user ID was saved - if that didn't work, this test is invalid
+      expect(session[:user_id]).must_equal user.id
+
+      # Act
+      get current_user_path
+
+      # Assert
+      must_respond_with :success
+    end
+  end
 end
