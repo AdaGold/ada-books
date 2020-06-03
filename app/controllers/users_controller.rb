6a29f97976c6087fa92bdac0a66103ae2fab6714
CheezItMan <chris@adadevelopersacademy.org>
2020-06-03 16:52:38 -0700

    UsersController#Create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,32 @@ class UsersController < ApplicationController
 
   def create
     auth_hash = request.env["omniauth.auth"]
-    binding.pry
+    # Try to find the user
+    user = User.find_by(uid: auth_hash[:uid], provider: "github")
+    if user
+      # User was found in the database
+      flash[:success] = "Logged in as returning user #{user.name}"
+    else
+      # User doesn't match anything in the DB
+      # Attempt to create a new user
+      user = User.build_from_github(auth_hash)
+
+      if user.save
+        flash[:success] = "Logged in as new user #{user.name}"
+      else
+        # Couldn't save the user for some reason. If we
+        # hit this it probably means there's a bug with the
+        # way we've configured GitHub. Our strategy will
+        # be to display error messages to make future
+        # debugging easier.
+        flash[:error] = "Could not create new user account: #{user.errors.messages}"
+        return redirect_to root_path
+      end
+    end
+
+    # If we get here, we have a valid user instance
+    session[:user_id] = user.id
+    return redirect_to root_path
   end
 
   def login
