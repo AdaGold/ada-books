2bf54289d354324ddeac51ae42d14d5c4ab13864
CheezItMan <chris@adadevelopersacademy.org>
2020-06-03 16:53:37 -0700

    built user from github

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  def self.build_from_github
+    user = User.new
+    user.uid = auth_hash[:uid]
+    user.provider = "github"
+    user.name = auth_hash["info"]["name"]
+    user.email = auth_hash["info"]["email"]
+
+    # Note that the user has not been saved.
+    # We'll choose to do the saving outside of this method
+    return user  
+  end
 end
