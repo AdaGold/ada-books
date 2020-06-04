22ce41fd460bc237fc94f02922998a7361cf327f
CheezItMan <chris@adadevelopersacademy.org>
2020-06-03 17:03:53 -0700

    change .name to .username in User#build_from_github

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  def self.build_from_github
+  def self.build_from_github(auth_hash)
     user = User.new
     user.uid = auth_hash[:uid]
     user.provider = "github"
-    user.name = auth_hash["info"]["name"]
+    user.username = auth_hash["info"]["name"]
     user.email = auth_hash["info"]["email"]
 
     # Note that the user has not been saved.
