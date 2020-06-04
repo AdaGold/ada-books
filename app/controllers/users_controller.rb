97a9e29cd301f1ebc8327a071508b29b9f859dd2
CheezItMan <chris@adadevelopersacademy.org>
2020-06-03 17:13:54 -0700

    remove unused code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,22 +32,6 @@ class UsersController < ApplicationController
     return redirect_to root_path
   end
 
-  def login
-    username = params[:user][:username]
-    user = User.find_by(username: username)
-    if user
-      session[:user_id] = user.id
-      flash[:success] = "Successfully logged in as returning user #{username}"
-    else
-      user = User.create(username: username)
-      session[:user_id] = user.id
-      flash[:success] = "Successfully logged in as new user #{username}"
-    end
-
-    redirect_to root_path
-    return
-  end
-
   def logout
     session[:user_id] = nil
     flash[:success] = "Successfully logged out"
--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,6 @@ Rails.application.routes.draw do
     resources :books, only: [:index, :new]
   end
 
-  get "/login", to: "users#login_form", as: "login"
-  post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"
   get "/users/current", to: "users#current", as: "current_user"
 
