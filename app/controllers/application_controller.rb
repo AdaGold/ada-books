03930ba8f906261140e955a9eae331b58c08cfa3
CheezItMan <chris@adadevelopersacademy.org>
2020-06-03 20:42:21 -0700

    update application controller filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,13 @@ class ApplicationController < ActionController::Base
 
   def current_user
     # return user matching id from session variable
-    return User.find_by(id: session[:user_id]) if session[:user_id]
+    return @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   def require_login
     if current_user.nil?
       flash[:error] = "You must be logged in to do that"
-      redirect_to login_path
+      redirect_to root_path
     end
   end
 
