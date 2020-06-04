db61fdefc7831df3a72dc9a00830ef6c284de862
CheezItMan <chris@adadevelopersacademy.org>
2020-06-03 20:41:37 -0700

    created omniauth.rb

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+# config/initializers/omniauth.rb
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"], scope: "user:email"
+end
